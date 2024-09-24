d40cd6699dd50c5bdef037c8cda3092b31f3979d
Razib031 <razibchakraborty7@gmail.com>
2024-09-24 14:19:20 +0600

    Create sp fast pro.cpp

---
+++ sp fast pro.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main {
+int num1,num2,sum;
+printf("Enter your fast number ");
+scanf("%d",num1);
+printf("Enter your second number");
+scanf("%d ",number2);
+sum = num1+num2;
+
+if (sum %2 == 0){
+    printf("The sum is even\n");
+}
+
+else {
+    printf(" The number is odd \n");
+}
+return 0;
+}
